24c08a663a0790587147bc208c561b78139aa5a7
guilherme6699 <guillherme.souza6699@gmail.com>
2025-10-24 23:13:06 -0300

    atualização nos cards de cada aba e adição de efeito de pagina carregada

--- assents/menu.js
+++ assents/menu.js
@@ -2,3 +2,10 @@ function toggleMenu(){
     const sidebar = document.getElementById('sidebar');
     sidebar.classList.toggle('active');
 }
+
+
+setTimeout(() => {
+    const overlay = document.getElementById('welcome');
+    overlay.classList.add('hidden');
+    setTimeout(() => overlay.remove(), 1000); 
+}, 3000);
